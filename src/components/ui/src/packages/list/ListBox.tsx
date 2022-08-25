5acd49dc218dca69359e945a3226a1c87f9c7f5a
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2022-08-25 12:35:55 +0100

    feat(List): Add list as result of the search

--- src/components/ui/src/packages/list/ListBox.tsx
+++ src/components/ui/src/packages/list/ListBox.tsx
@@ -1,5 +1,18 @@
 import styled from 'styled-components'
 
-const ListBox = styled.ul``
+const ListBox = styled.ul<{ isOpen: boolean }>`
+    width: 100%;
+    max-width: 50rem;
+    background-color: #1a191c;
+    padding: 2rem 0;
+    border-radius: 0.5rem;
+    transform: ${({ isOpen }) => (isOpen ? 'scaleY(1)' : 'scaleY(0)')}
+        translateY(0.05rem);
+    transform-origin: top;
+    transition: transform 0.2s ease;
+
+    position: absolute;
+    top: 7rem;
+`
 
 export default ListBox
--- src/components/ui/src/packages/list/ListItem.tsx
+++ src/components/ui/src/packages/list/ListItem.tsx
@@ -1,5 +1,19 @@
 import styled from 'styled-components'
 
-const ListItem = styled.ul``
+const ListItem = styled.li`
+    cursor: pointer;
+    list-style: none;
+    padding: 1rem 2rem;
+    background-color: #1a191c;
+    transition: filter 0.2s;
+
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+
+    &:hover {
+        filter: brightness(2);
+    }
+`
 
 export default ListItem
